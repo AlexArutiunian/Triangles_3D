eac1b3006abf96c733e7bd3c81508c77c7afabfa
AlexArutiunian <arutunanalexandr17@gmail.com>
2023-04-04 20:44:19 +0300

    upd readme

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
 # Triangle_3D
 
-[Is point in triangle?](http://cyber-code.ru/tochka_v_treugolnike/)
+Input: N - number of triangles and N ones of triangles in 3D.
+
+Output: numbers of triangles that intersect with any other. 
+
+Input:
+``` bash
+6
+1 0 0 0 1 0 0 0 1
+3 0 0 0 3 0 0 0 3
+0 0 0 0 0 4 0 4 0
+0 0 2 4 1 0 1 4 0
+0 0 10 3 7 0 7 3 0
+0 -4 0 0 -6 0 2 -5 0
+
+```
+Output:
+```bash
+3 4
+```
+Or output:
+```bash
+4 3
+```
+P.S (4 3 also is right)
--- triang.cpp
+++ triang.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 #include "lib/intersection.hpp"
 #include "lib/geom.hpp"
 
@@ -16,16 +17,37 @@ int main(){
         std::cin >> triangles[i];
     }
 
-    unsigned int count = 0;
+    std::vector<unsigned int> num_int;
+    std::unordered_map<unsigned int, unsigned int> num_intersect;
 
     for(int i = 0; i != n - 1; ++i){
         for(int j = i; j != n - 1; ++j){
-            if(geom::is_intersect_triangles_3D(triangles[j], triangles[j + 1]))
-                count += 1;
+            if(geom::is_intersect_triangles_3D(triangles[j], triangles[j + 1])){
+                auto founder_fst = num_intersect.find(j);
+                auto founder_snd = num_intersect.find(j + 1);
+                if(founder_fst == num_intersect.end()){
+                    num_intersect[j] = j + 1;
+                }
+                if(founder_snd == num_intersect.end()){
+                    num_intersect[j + 1] = j + 2;
+                }
+            }
+                
         }        
     }
-
-    std::cout << count << std::endl;
-
-    
+    for(auto x : num_intersect)
+        std::cout << x.second << ' ';
+    std::cout << '\n';    
 }
+/*
+6
+1 0 0 0 1 0 0 0 1
+3 0 0 0 3 0 0 0 3
+0 0 0 0 0 4 0 4 0
+0 0 2 4 1 0 1 4 0
+0 0 10 3 7 0 7 3 0
+0 -4 0 0 -6 0 2 -5 0
+
+except: 3 4
+
+*/
